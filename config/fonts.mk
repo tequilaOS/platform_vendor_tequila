5c660c6e38fc0b45bf00964d42ff96830e269b5f
Arseniy Graur <argraur@aosip.dev>
2020-02-12 09:52:38 +0000

    vendor: Import and set GoogleSans as default font
    Change-Id: Ia8a1c810942c94eb6d3ae9e1b44a93dbc0846939

---
+++ config/fonts.mk
@@ -0,0 +1,7 @@
+# google-sans family
+PRODUCT_PACKAGES += \
+    GoogleSans-Italic.ttf \
+    GoogleSans-Regular.ttf
+
+PRODUCT_COPY_FILES += \
+    vendor/aosp/fonts/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml
