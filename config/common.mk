c74e09c27b73896017e7acdf845bc2fb6ad869ff
jhenrique09 <jhsv09@gmail.com>
2022-02-10 11:25:28 -0300

    vendor: config: Add support for Face Unlock
    Change-Id: I0beb2311a7fa469182b71c54f0773aac36b53354

--- config/common.mk
+++ config/common.mk
@@ -104,6 +104,17 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     SimpleDeviceConfig
 
+# Face Unlock
+TARGET_FACE_UNLOCK_SUPPORTED ?= true
+ifeq ($(TARGET_FACE_UNLOCK_SUPPORTED),true)
+PRODUCT_PACKAGES += \
+    FaceUnlockService
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.face_unlock_service.enabled=$(TARGET_FACE_UNLOCK_SUPPORTED)
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
+endif
+
 # Extra tools in tequila
 PRODUCT_PACKAGES += \
     7z \
