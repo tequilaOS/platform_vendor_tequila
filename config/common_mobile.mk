a35757718dc668dd6b0667fa90ec8b6755ea8f56
Bartłomiej Rudecki <ejbtrd@gmail.com>
2022-06-11 01:32:24 +0200

    Remove Lineage charger

--- config/common_mobile.mk
+++ config/common_mobile.mk
@@ -32,16 +32,6 @@ PRODUCT_DEXPREOPT_SPEED_APPS += \
     TrebuchetQuickStep
 endif
 
-# Charger
-PRODUCT_PACKAGES += \
-    charger_res_images
-
-ifneq ($(WITH_LINEAGE_CHARGER),false)
-PRODUCT_PACKAGES += \
-    lineage_charger_animation \
-    lineage_charger_animation_vendor
-endif
-
 # Customizations
 PRODUCT_PACKAGES += \
     IconPackCircularAndroidOverlay \
