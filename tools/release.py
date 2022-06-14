92a898a80ca731a1b8605929c274893972662e94
Bartłomiej Rudecki <ejbtrd@gmail.com>
2022-06-14 12:08:24 +0200

    vendor: releaser: Update & cleanup
    * Remove telegram message
    * Cleanup unused imports
    * Update ota repository variable to unique name
    * Print link to release
    * Create getProp function
    * Print "Released" message earlier, so it's always shown
    * Update messages

--- tools/release.py
+++ tools/release.py
@@ -5,22 +5,15 @@ import json
 import os
 import sys
 
-from jinja2 import Undefined
-
 try:
     from github import Github, GithubException
 except ImportError:
-    sys.exit("Please install pygithub package via pip")
+    sys.exit("E: Please install pygithub package via pip")
 
 try:
     from git import Repo
 except ImportError:
-    sys.exit("Please install gitpython package via pip")
-
-try:
-    import telegram_send
-except ImportError:
-    sys.exit("Please install telegram-send package via pip")
+    sys.exit("E: Please install gitpython package via pip")
 
 home = str(Path.home())
 token = str(open(home + "/.githubtoken", "r").read().strip())
@@ -34,10 +27,10 @@ try:
     zipName = zip.split("/")[-1]
 
     if "tequila" not in zipName and ("-OFFICIAL" not in zipName or "-EXPERIMENTAL" not in zipName) and ".zip" not in zipName:
-        sys.exit("Incorrect file!")
+        sys.exit("E: Incorrect file!")
 
 except IndexError:
-    sys.exit("Incorrect file!")
+    sys.exit("E: Incorrect file!")
 
 codename = zipName.split("-")[5].replace(".zip", "")
 date = (zipName.split("-")[2] + "-" + zipName.split("-")[3]).split(".")[0]
@@ -47,38 +40,43 @@ if zipName.split("-")[4] == "EXPERIMENTAL":
 else:
     isExperimental = False
 
-print("Releasing " + date + " build for " + codename)
+print("I: Releasing " + date + " build for " + codename)
 
 repo = None
 repos = g.get_organization("tequilaOS").get_repos()
 for r in repos:  
     if codename in r.name and "platform_device_" in r.name:
         repo = r
-        print("Repo found for your device: " + repo.name)
+        print("I: Repo found for your device: " + repo.name)
         break
 
 if not repo:
-    sys.exit("\nERROR: Can't find repo for " + codename) 
+    sys.exit("\nE: Can't find repo for " + codename) 
 
 tag = date
 title = "tequila-" + tag
 
 try:
     release = repo.create_git_release(tag, title, "Automated release of " + zipName, prerelease=isExperimental)
-    print("Uploading asset...")
+    print("I: Uploading asset...")
     release.upload_asset(zip)
-    print("Asset uploaded!")
+    print("I: Asset uploaded!")
 except GithubException:
-    sys.exit("Release already exists!")
+    sys.exit("E: Release already exists!")
+
+print("Released " + title + "!")
+print("https://github.com/tequilaOS/" + repo.name + "/releases/tag/" + tag)
 
 if isExperimental:
-    sys.exit("Release is experimental, OTA config and telegram message are not going to be created.")
+    sys.exit("W: Release is experimental, OTA config and telegram message are not going to be created.")
 
-props = open(OUT + "/system/build.prop", "r").read().splitlines()
-for line in props:
-    if "ro.build.date.utc" in line:
-        datetime = int(line.replace("ro.build.date.utc=", ""))
+def getProp(prop):
+    props = open(OUT + "/system/build.prop", "r").read().splitlines()
+    for line in props:
+        if prop in line:
+            return line.replace(prop + "=", "")
 
+datetime = int(getProp("ro.build.date.utc"))
 url = "https://github.com/tequilaOS/" + repo.name + "/releases/download/" + tag + "/" + zipName
 checksum = open(zip + ".sha256sum", "r").read().split(" ")[0]
 filesize = os.path.getsize(zip)
@@ -101,24 +99,7 @@ jsonFile = open(ANDROID_BUILD_TOP + "/tequila_ota/devices/" + codename + ".json"
 jsonFile.write(json.dumps(template, indent=2))
 jsonFile.close()
 
-repo = Repo("tequila_ota")
-repo.git.add("devices/" + codename + ".json")
-repo.index.commit("ota: " + codename + "-" + date)
-repo.remote(name="tequila").push()
-
-print("Released " + title + "!")
-
-date = zipName.split("-")[2]
-date = date[0:4] + "/" + date[4:6] + "/" + date[6:8]
-
-message = (
-    f"<b>tequilaOS | 12.1</b>\n"
-    f"<b>Device:</b> {codename}\n"
-    f"<b>Date:</b> {date}\n"
-    f"\n"
-    f"<a href='https://tequilaOS.pl/download?device={codename}'>Download</a>\n"
-    f"\n"
-    f"@tequilaOS | @tequilaOSChat"
-)
-
-telegram_send.send(conf="~/.config/tequilatg.conf", parse_mode="html", messages=[message])
+ota_repo = Repo("tequila_ota")
+ota_repo.git.add("devices/" + codename + ".json")
+ota_repo.index.commit("ota: " + codename + "-" + date)
+ota_repo.remote(name="tequila").push()
