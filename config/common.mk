22c043f1794b6efd822cbdbf70bbb0377b9aa22f
Bartłomiej Rudecki <ejbtrd@gmail.com>
2022-08-16 20:10:40 +0000

    [DNM] vendor: Do not include `vendor/google/*`
    Change-Id: I48c2ef3d328b71f20de15141dd64cec64125103b

--- config/common.mk
+++ config/common.mk
@@ -197,6 +197,6 @@ $(call inherit-product, vendor/tequila/config/version.mk)
 
 $(call inherit-product, vendor/tequila/config/sepolicy.mk)
 
-$(call inherit-product, vendor/google/gms/config.mk)
+#$(call inherit-product, vendor/google/gms/config.mk)
 
-$(call inherit-product, vendor/google/pixel/config.mk)
+#$(call inherit-product, vendor/google/pixel/config.mk)
