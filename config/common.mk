2517b62a146387775ba4c279fd73336ca42ae03b
Aaron Kling <webgeek1234@gmail.com>
2022-09-13 19:49:47 -0500

    config: Remove missing cli tools
    These have not been ported for several versions
    
    Change-Id: I40f199b6628e757e2637814b96f20bfb537393a4

--- config/common.mk
+++ config/common.mk
@@ -118,18 +118,13 @@ PRODUCT_PRODUCT_PROPERTIES += \
 
 # Extra tools in tequila
 PRODUCT_PACKAGES += \
-    7z \
     bash \
     curl \
     getcap \
     htop \
-    lib7z \
     nano \
-    pigz \
     setcap \
-    unrar \
-    vim \
-    zip
+    vim
 
 PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
     system/bin/curl \
@@ -148,10 +143,6 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     vendor/tequila/prebuilt/common/etc/init/init.openssh.rc:$(TARGET_COPY_OUT_PRODUCT)/etc/init/init.openssh.rc
-
-# rsync
-PRODUCT_PACKAGES += \
-    rsync
 
 # Storage manager
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
