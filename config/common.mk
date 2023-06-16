23826e3eb7ce8c8ea11b32e458220a4540af8b16
Bartłomiej Rudecki <ejbtrd@gmail.com>
2023-06-16 12:12:54 +0200

    vendor: Update gapps path
    Change-Id: Ifc2a508afedae90413870a57cb6ebc57c713fef0

--- config/common.mk
+++ config/common.mk
@@ -174,6 +174,4 @@ $(call inherit-product, vendor/tequila/config/fonts.mk)
 
 $(call inherit-product, vendor/tequila/config/version.mk)
 
-$(call inherit-product, vendor/google/gms/config.mk)
-
-$(call inherit-product, vendor/google/pixel/config.mk)
+$(call inherit-product, vendor/gapps/common.mk)
