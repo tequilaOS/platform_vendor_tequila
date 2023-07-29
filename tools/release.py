bf7f53e0dbb6598b8e2c6c94f4f78be9870d5404
Bartłomiej Rudecki <ejbtrd@gmail.com>
2023-07-29 14:18:34 +0200

    releaser: Reformat the file
    Change-Id: Iad0c8e4f86ddb6f8ac07a4216f42e3fa1b5b494b

--- tools/release.py
+++ tools/release.py
@@ -35,7 +35,11 @@ try:
     zip = os.path.abspath(sys.argv[1])
     zipName = zip.split("/")[-1]
 
-    if "tequila" not in zipName and ("-OFFICIAL" not in zipName or "-EXPERIMENTAL" not in zipName) and ".zip" not in zipName:
+    if (
+        "tequila" not in zipName
+        and ("-OFFICIAL" not in zipName or "-EXPERIMENTAL" not in zipName)
+        and ".zip" not in zipName
+    ):
         print("E: Incorrect file!")
         sys.exit(1)
 
@@ -55,30 +59,29 @@ print("I: Releasing " + date + " build for " + codename)
 
 repo = None
 repos = g.get_organization("tequilaOS").get_repos()
-for r in repos:  
+for r in repos:
     if codename in r.name and "platform_device_" in r.name:
         repo = r
         print("I: Repo found for your device: " + repo.name)
         break
 
 if not repo:
-    print("\nE: Can't find repo for " + codename) 
+    print("\nE: Can't find repo for " + codename)
     sys.exit(1)
 
 tag = date
 title = zipName.split("-")[1] + "-" + tag
 
-additional_files = [
-    "recovery",
-    "boot",
-    "dtbo",
-    "vendor_boot"
-]
+additional_files = ["recovery", "boot", "dtbo", "vendor_boot"]
 
-additional_files_path = OUT + "/obj/PACKAGING/target_files_intermediates/tequila_" + codename + "*/IMAGES/"
+additional_files_path = (
+    OUT + "/obj/PACKAGING/target_files_intermediates/tequila_" + codename + "*/IMAGES/"
+)
 
 try:
-    release = repo.create_git_release(tag, title, "Automated release of " + zipName, prerelease=isExperimental)
+    release = repo.create_git_release(
+        tag, title, "Automated release of " + zipName, prerelease=isExperimental
+    )
     print("I: Uploading build...")
     release.upload_asset(zip)
     for file_name in additional_files:
@@ -90,7 +93,7 @@ try:
             pass
     print("I: Assets uploaded!")
 except GithubException as error:
-    print("E: Failed creating release: " + error.data['errors'][0]['code'])
+    print("E: Failed creating release: " + error.data["errors"][0]["code"])
     sys.exit(1)
 
 print("Released " + title + "!")
@@ -100,14 +103,23 @@ if isExperimental:
     print("W: Release is experimental, skipping OTA config generation!")
     sys.exit(0)
 
+
 def getProp(prop):
     with open(OUT + "/system/build.prop", "r") as props:
         for line in props.read().splitlines():
             if prop in line:
                 return line.replace(prop + "=", "")
 
+
 datetime = int(getProp("ro.build.date.utc"))
-url = "https://github.com/tequilaOS/" + repo.name + "/releases/download/" + tag + "/" + zipName
+url = (
+    "https://github.com/tequilaOS/"
+    + repo.name
+    + "/releases/download/"
+    + tag
+    + "/"
+    + zipName
+)
 with open(zip + ".sha256sum", "r") as f:
     checksum = f.read().split(" ")[0]
 filesize = os.path.getsize(zip)
@@ -121,15 +133,20 @@ template = {
       "id": checksum,
       "size": filesize,
       "url": url,
-      "version": version
+      "version": version,
     }
   ]
 }
 
-with open(ANDROID_BUILD_TOP + "/tequila_ota/devices/" + codename + ".json", "w") as jsonFile:
+with open(
+    ANDROID_BUILD_TOP + "/tequila_ota/devices/" + codename + ".json", "w"
+) as jsonFile:
     jsonFile.write(json.dumps(template, indent=2))
 
 ota_repo = Repo(ANDROID_BUILD_TOP + "/tequila_ota")
 ota_repo.git.add("devices/" + codename + ".json")
 sha = ota_repo.index.commit("ota: " + codename + "-" + date + "\n")
-ota_repo.git.push("ssh://" + GERRIT_USERNAME + "@review.tequilaos.org:29418/tequilaOS/tequila_ota", str(sha) + ":refs/for/main")
+ota_repo.git.push(
+    "ssh://" + GERRIT_USERNAME + "@review.tequilaos.org:29418/tequilaOS/tequila_ota",
+    str(sha) + ":refs/for/main",
+)
