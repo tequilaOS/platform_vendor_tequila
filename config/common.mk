e64e174ea1c15fd8ad20906d517203f69f5c767e
Bartłomiej Rudecki <ejbtrd@gmail.com>
2023-10-11 22:08:33 +0200

    vendor: Drop GoogleSans
    * Moved to GMS
    
    Change-Id: I3411aa748c77ae7bf510fd84135c635f3c9dd081

--- config/common.mk
+++ config/common.mk
@@ -182,7 +182,6 @@ PRODUCT_PACKAGES += \
     Updater
 endif
 
-$(call inherit-product, vendor/tequila/config/fonts.mk)
 $(call inherit-product, vendor/tequila/config/version.mk)
 $(call inherit-product, vendor/google/gms/config.mk)
 $(call inherit-product, vendor/google/pixel/config.mk)
