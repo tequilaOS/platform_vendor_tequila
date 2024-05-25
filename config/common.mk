de4e2dc0ec9270bf8877c273d109d5418455d62f
Bartłomiej Rudecki <przekichane@icloud.com>
2024-05-25 04:42:02 +0200

    config: Log privapp-permissions whitelist

--- config/common.mk
+++ config/common.mk
@@ -80,9 +80,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/lineage/config/permissions/org.lineageos.android.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/org.lineageos.android.xml
 
-# Enforce privapp-permissions whitelist
+# Log privapp-permissions whitelist
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    ro.control_privapp_permissions=enforce
+    ro.control_privapp_permissions=log
 
 ifneq ($(TARGET_DISABLE_LINEAGE_SDK), true)
 # Lineage SDK
